416a0403d7a0b43c1c19177fd34bba80f24ec269
christoff-linde <christoff.linde@gmail.com>
2024-10-25 17:46:08 +0200

    feat(cmd/server/http): migrate http api code into cmd directory

---
+++ cmd/server/http/models.go
@@ -0,0 +1,24 @@
+package http
+
+import (
+	"time"
+
+	"github.com/jackc/pgx/v5/pgtype"
+)
+
+type Sensor struct {
+	CreatedAt  time.Time `json:"created_at"`
+	UpdatedAt  time.Time `json:"updated_at"`
+	SensorName string    `json:"sensor_name"`
+	ID         int32     `json:"id,omitempty"`
+}
+
+type SensorMetadata struct {
+	ID             int32       `json:"id"`
+	SensorID       int32       `json:"sensor_id"`
+	SensorType     pgtype.Text `json:"sensor_type"`
+	Manufacturer   pgtype.Text `json:"manufacturer"`
+	ModelNumber    pgtype.Text `json:"model_number"`
+	SensorLocation pgtype.Text `json:"sensor_location"`
+	AdditionalData []byte      `json:"additional_data"`
+}
