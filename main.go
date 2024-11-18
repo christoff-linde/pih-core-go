057bf68a3b154747f95b353123d9c1ebdc29307d
christoff-linde <christoff.linde@gmail.com>
2024-11-18 22:45:47 +0200

    feat(main): embed migrations into binary
    the migrations are now embedded at compile time, and will run at startup
    against the databse.

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"context"
+	"database/sql"
+	"embed"
 	"fmt"
 	"log"
 	"os"
@@ -11,7 +13,9 @@ import (
 	"github.com/christoff-linde/pih-core-go/cmd/subscriber"
 	db "github.com/christoff-linde/pih-core-go/internal/database"
 	"github.com/jackc/pgx/v5/pgxpool"
+	_ "github.com/jackc/pgx/v5/stdlib"
 	"github.com/joho/godotenv"
+	"github.com/pressly/goose/v3"
 )
 
 func initDb(databaseUrl string) *db.Queries {
@@ -22,6 +26,25 @@ func initDb(databaseUrl string) *db.Queries {
 
 	db := db.New(connection)
 	return db
+}
+
+//go:embed db/schema/*.sql
+var embedMigrations embed.FS
+
+func runMigrations(databaseUrl string) {
+	sqlDB, err := sql.Open("pgx", databaseUrl)
+	if err != nil {
+		log.Fatal(err, "Unable to create sql.DB connection: %v\n", err)
+	}
+	defer sqlDB.Close()
+
+	goose.SetBaseFS(embedMigrations)
+	if err := goose.SetDialect("postgres"); err != nil {
+		log.Fatal(err)
+	}
+	if err := goose.Up(sqlDB, "db/schema"); err != nil {
+		log.Fatal(err)
+	}
 }
 
 func main() {
@@ -36,7 +59,10 @@ func main() {
 	databaseUrl := os.Getenv("DB_URL")
 	fmt.Println("Found DB_URL with value:", databaseUrl)
 
+	runMigrations(databaseUrl)
+
 	dbConn := initDb(databaseUrl)
+
 	subscriberCfg := subscriber.AppConfig{DB: dbConn}
 
 	go subscriber.StartSubscriber(subscriberCfg, brokerUrl)
