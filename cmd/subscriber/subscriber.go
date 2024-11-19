8b6c14ef0ec3ba632cf2a1b3c218975d71d89c23
christoff-linde <christoff.linde@gmail.com>
2024-11-19 19:50:09 +0200

    feat(cmd/subscriber): add func to extract timestamp from header
    the timestamp is automatically added for each message upon receive by
    RabbitMQ, and is now extracted to be written into the DB. this tries to
    ensure that the timestamps are not incorrect when writing to the DB when
    (if) the backend consumer service should stop processing.

--- cmd/subscriber/subscriber.go
+++ cmd/subscriber/subscriber.go
@@ -3,8 +3,10 @@ package subscriber
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
+	"time"
 
 	db "github.com/christoff-linde/pih-core-go/internal/database"
 	"github.com/jackc/pgx/v5/pgtype"
@@ -25,6 +27,41 @@ func failOnError(err error, msg string) {
 	if err != nil {
 		log.Panicf("%s: %s", msg, err)
 	}
+}
+
+func ExtractTimestamp(d amqp.Delivery) (time.Time, error) {
+	const timestampHeaderKey = "timestamp_in_ms"
+
+	if d.Headers == nil {
+		return time.Time{}, errors.New("headers are nil")
+	}
+
+	// Check if the timestamp header exists
+	if timestampValue, ok := d.Headers[timestampHeaderKey]; ok {
+		// Convert the timestamp to time.Time
+		switch v := timestampValue.(type) {
+		case time.Time:
+			log.Printf("Time format")
+			return v, nil
+		case int64:
+			log.Printf("int64 format")
+			return time.Unix(0, v*int64(time.Millisecond)), nil
+		case float64:
+			log.Printf("float64 format")
+			return time.Unix(0, int64(v)*int64(time.Millisecond)), nil
+		case string:
+			log.Printf("string format")
+			parsedTime, err := time.Parse(time.RFC3339, v)
+			if err != nil {
+				return time.Time{}, err
+			}
+			return parsedTime, nil
+		default:
+			return time.Time{}, errors.New("unsupported timestamp format")
+		}
+	}
+
+	return time.Time{}, errors.New("timestamp header not found")
 }
 
 func StartSubscriber(appCfg AppConfig, brokerUrl string) {
@@ -54,8 +91,18 @@ func StartSubscriber(appCfg AppConfig, brokerUrl string) {
 		for d := range iotMsgs {
 			log.Printf("Received a message from: %v: %v", iotQueue.Name, d.Body)
 
+			// extract timestamp from headers
+			timestamp, err := ExtractTimestamp(d)
+			if err != nil {
+				log.Printf("Failed to extract timestamp: %v", err)
+			}
+
+			// parse message body as json
 			var deviceData DeviceData
-			err := json.Unmarshal([]byte(d.Body), &deviceData)
+			err = json.Unmarshal([]byte(d.Body), &deviceData)
+			if err != nil {
+				log.Printf("Error parsing JSON: %v", err)
+			}
 			if err != nil {
 				log.Printf("Error parsing JSON: %v", err)
 			}
@@ -66,6 +113,10 @@ func StartSubscriber(appCfg AppConfig, brokerUrl string) {
 			} else {
 				if deviceData.Temperature > 0 || deviceData.Humidity > 0 {
 					sensorReading, err := appCfg.DB.CreateSensorReading(context.Background(), db.CreateSensorReadingParams{
+						ReadingTimestamp: pgtype.Timestamptz{
+							Time:  timestamp,
+							Valid: true,
+						},
 						SensorID: pgtype.Int4{
 							Int32: sensor.ID,
 							Valid: true,
